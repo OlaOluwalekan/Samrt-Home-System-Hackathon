032c909e3ccadca1c71f92ee290be58bb6614a05
OlaOluwalekan <olalekanbello534@gmail.com>
2023-10-06 17:12:38 +0100

    completed hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import Hero from '../components/hero/Hero'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      {/* HERO SECTION BELOW */}
+      <Hero />
+
+      {/* ABOUT US SECTION BELOW */}
+
+      {/* TESTIMONIAL SECTION BELOW */}
+
+      {/* APP DOWNLOAD SECTION BELOW */}
+    </div>
+  )
 }
 export default Home
