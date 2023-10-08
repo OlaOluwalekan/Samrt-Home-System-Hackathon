da656383e37c0763ddff776049dc2ff650307d11
Loisyy <nkeiruloisy@yahoo.com>
2023-10-08 12:20:14 +0100

    Testimonial section done

---
+++ client/src/components/testimonial/TestimonyCard.jsx
@@ -0,0 +1,20 @@
+import {FaTwitter} from 'react-icons/fa'
+
+
+const TestimonyCard = ({name, text, photo, handle}) =>{
+    return (
+    <div className="grid-item">
+  <p>{text}</p>
+  <section className='content'>
+    <img src={photo} alt={name} className='image'/>
+    <article>
+      <p>{name}</p>
+      <p>@{handle}</p>
+    </article>
+    <span><FaTwitter/></span>
+  </section>
+</div>
+  )
+}
+
+export default TestimonyCard
