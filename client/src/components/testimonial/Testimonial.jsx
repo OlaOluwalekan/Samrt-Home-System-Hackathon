b6d58175a1baf2f192fc20b37dd704b24f4d097a
OlaOluwalekan <olalekanbello534@gmail.com>
2023-10-08 15:00:25 +0100

    some adjustments to testimonials

--- client/src/components/testimonial/Testimonial.jsx
+++ client/src/components/testimonial/Testimonial.jsx
@@ -1,30 +1,18 @@
-import { testimonies } from "./testimonial2";
-import "./Testimonial.css"
-import TestimonyCard from "./TestimonyCard"
-import Ellipse5 from '../../assets/images/Ellipse 5.jpg';
-import Ellipse6 from '../../assets/images/Ellipse 6.jpg';
-import Ellipse7 from '../../assets/images/Ellipse 7.jpg';
-import Ellipse8 from '../../assets/images/Ellipse 8.jpg';
+import { testimonies } from './testimonial2'
+import './Testimonial.css'
+import TestimonyCard from './TestimonyCard'
 
-
-
-const Testimonial = () =>{
+const Testimonial = () => {
   return (
-  // <div>Testimonial</div>
-  <div className="container">
-      {
-        testimonies.map((testimony) => {
-    return <TestimonyCard key={testimony.id} {...testimony} />
-})
-      }
-    </div>
-  );
+    <>
+      <h2>What do people say about us?</h2>
+      <div className='container' id='testimonials'>
+        {testimonies.map((testimony) => {
+          return <TestimonyCard key={testimony.id} {...testimony} />
+        })}
+      </div>
+    </>
+  )
 }
 
-
-
-
-
-export default Testimonial;
-
-
+export default Testimonial
--- client/src/components/testimonial/TestimonyCard.jsx
+++ client/src/components/testimonial/TestimonyCard.jsx
@@ -1,19 +1,20 @@
-import {FaTwitter} from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
 
-
-const TestimonyCard = ({name, text, photo, handle}) =>{
-    return (
-    <div className="grid-item">
-  <p>{text}</p>
-  <section className='content'>
-    <img src={photo} alt={name} className='image'/>
-    <article>
-      <p>{name}</p>
-      <p>@{handle}</p>
-    </article>
-    <span><FaTwitter/></span>
-  </section>
-</div>
+const TestimonyCard = ({ name, text, photo, handle }) => {
+  return (
+    <div className='grid-item'>
+      <p>{text}</p>
+      <section className='content'>
+        <img src={photo} alt={name} className='image' />
+        <article>
+          <p>{name}</p>
+          <p>@{handle}</p>
+        </article>
+        <span>
+          <FaTwitter />
+        </span>
+      </section>
+    </div>
   )
 }
 
